bf16bcfb8ceb974881aefdbe78f227fa55d435d5
Małgorzata Kapuścińska <m_kapuscinska1@o2.pl>
2018-04-27 14:27:36 +0200

    Add countries-reducer and combine reducers in index.js

---
+++ src/reducers/countries-reducer.js
@@ -0,0 +1,15 @@
+import {GET_COUNTRIES} from '../src/actions/actions-countries';
+import countriesData from '..src/data/countries.json';
+
+const initialState = {
+  countries: countriesData
+};
+
+const countriesReducer = function(state: initialState, action) {
+  switch(action.type){
+    case GET_COUNTRIES:
+      return Object.assign(state, {}, {countries: state.countries});
+  }
+
+  return state;
+};
