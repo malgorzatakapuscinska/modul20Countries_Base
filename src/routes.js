dc1c90358f20cb5238e9b7effb71acef8b706549
Małgorzata Kapuścińska <m_kapuscinska1@o2.pl>
2018-04-28 07:18:58 +0200

    Add routes for Home, Contact, NotFound components

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router';
 import Navigation from './presentational/navigation.component';
+import Contact from './presentational/contact_component';
+import Home from './presentational/home_component';
+import NotFound from './presentational/not_found_component.js';
 
 export default (
   <Route path='/' component={Navigation}>
+    <IndexRoute component={Home} />
+    <Route path='contact' component={Contact} />
+    <Route path='*' component={NotFound} />
 
   </Route>
 );
